456f42024541b344207177d7936097643a175335
james-coombs <coombsj4@yahoo.com>
2017-08-06 18:54:52 -0400

    Update Readme and app Name

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ require 'action_controller/railtie'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module WemberApi
+module YambaApi
   # :nodoc:
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified
